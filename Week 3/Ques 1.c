e743166c74d15cd70dace3b5da88ddbc5722263e
ManavChauhan1 <manavchauhan07@gmail.com>
2021-11-11 13:17:30 +0530

    Update Ques 1.c

--- Week 3/Ques 1.c
+++ Week 3/Ques 1.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 void main()
 {
-    int n,row,count;
-    printf("Enter n:");
-    scanf("%d",&n);
+    int n=5,row,count;
     for(row=1;row<=n;row++)
     {
         for(count=1;count<=row;count++)
         {
-            printf("%d",count);
+            printf("*");
         }
         printf("\n");
     }
