3ad06706b560d8282309dbecedf9012b0be3a4e0
ManavChauhan1 <manavchauhan07@gmail.com>
2021-11-14 21:01:36 +0530

    Update Ques 2.c

--- Week 4/Ques 2.c
+++ Week 4/Ques 2.c
@@ -12,20 +12,25 @@ void main()
     }
     for(i=0;i<n;i++)
     {
-          count=0;
-          for(j=n-1;j>=0;j--)
-           {
-                 if(a[i]==a[j])
-                  {
+        count=1;
+        if(a[i]!=-1)
+        {
+            for(j=i+1;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
                     count++;
-                  }
-                  freq[i]=count;
-           }
+                    a[j]=-1;
+                }
+                freq[i]=count;
+            }
 
+        }
     }
     for(i=0;i<n;i++)
     {
-        printf("\nFrequency of %d is:%d",a[i],freq[i]);
+        if(a[i]!=-1)
+            printf("\nFrequency of %d is:%d",a[i],freq[i]);
     }
 }
 
